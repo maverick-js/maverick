62fcb09d7e2a79c4af6983349aeb7752572ec2c7
Rahim Alwer <rahim.alwer@gmail.com>
2022-12-01 20:14:30 +1100

    fix: `useHost` should return element instance host

--- packages/maverick/src/std/host.ts
+++ packages/maverick/src/std/host.ts
@@ -1,12 +1,19 @@
 import { getElementInstance } from '../element/internal';
-import type { AnyMaverickElement } from '../element/types';
+import type {
+  AnyMaverickElement,
+  ElementInstanceHost,
+  InferElementEvents,
+  InferElementProps,
+} from '../element/types';
 
-export function useHost<Element extends AnyMaverickElement>(): Element {
+export function useHost<
+  Element extends AnyMaverickElement = AnyMaverickElement,
+>(): ElementInstanceHost<InferElementProps<Element>, InferElementEvents<Element>> {
   const instance = getElementInstance();
 
   if (!instance) {
     throw Error(__DEV__ ? '[maverick] called `useHost` outside of root or setup function' : '');
   }
 
-  return instance.host as unknown as Element;
+  return instance.host;
 }

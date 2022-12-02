ad2b10dd0a8901932626eafd106c9ee6aa0e5896
Rahim Alwer <rahim.alwer@gmail.com>
2022-12-02 17:25:36 +1100

    fix: remove `useHost`

--- packages/maverick/src/std/host.ts
+++ packages/maverick/src/std/host.ts
@@ -1,21 +1,4 @@
 import { getElementInstance } from '../element/internal';
-import type {
-  AnyCustomElement,
-  CustomElementInstanceHost,
-  HTMLCustomElement,
-} from '../element/types';
-
-export function useHost<
-  Element extends AnyCustomElement = HTMLCustomElement,
->(): CustomElementInstanceHost<Element> {
-  const instance = getElementInstance();
-
-  if (!instance) {
-    throw Error(__DEV__ ? '[maverick] called `useHost` outside of root or setup function' : '');
-  }
-
-  return instance.host as CustomElementInstanceHost<Element>;
-}
 
 export function useHostConnected(): () => boolean {
   const instance = getElementInstance();

d4e905ff7bbf215a23304534953a6bc4dc842d1d
Rahim Alwer <rahim.alwer@gmail.com>
2024-10-18 14:49:38 +1100

    fix: remove deprecated event functions

--- packages/dom/src/runtime.ts
+++ packages/dom/src/runtime.ts
@@ -1,7 +1,6 @@
 import {
   cloneEvent,
   isArray,
-  isDOMNode,
   isFunction,
   isHTMLElement,
   isString,
@@ -19,7 +18,6 @@ import {
   type ComponentConstructor,
   computed,
   createComponent,
-  createEventTarget,
   CUSTOM_ELEMENT_SYMBOL,
   type CustomElement,
   effect,
